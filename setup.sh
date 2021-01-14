82d2abdc30997886139c107c7c5653be4900d4ad
z-gursky <66225970+z-gursky@users.noreply.github.com>
2021-01-14 15:07:16 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,9 +1,9 @@
 mkdir -p ~/.streamlit/
-
-echo "\
-[server]\n\
-port = $PORT\n\
-enableCORS = false\n\
-headless = true\n\
-\n\
+echo "[general]
+email = \"gursky.zach@gmail.com\"
+" > ~/.streamlit/credentials.toml
+echo "[server]
+headless = true
+port = $PORT
+enableCORS = false
 " > ~/.streamlit/config.toml
